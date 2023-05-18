6ddd7d88e2202fb96e0ae28c8ba212a884886074
togawa427 <95838999+togawa427@users.noreply.github.com>
2023-05-18 11:03:28 +0900

    test: 検証として動作にあまり影響がない部分を一部変えてみた

---
+++ StayWatchBeaconiOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StayWatchBeaconiOS
+//
+//  Created by 戸川浩汰 on 2023/05/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, Beacon!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StayWatchBeaconiOS/StayWatchBeaconiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayWatchBeaconiOSApp.swift
+//  StayWatchBeaconiOS
+//
+//  Created by 戸川浩汰 on 2023/05/18.
+//
+
+import SwiftUI
+
+@main
+struct StayWatchBeaconiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
